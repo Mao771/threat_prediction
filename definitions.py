167dbd3f30a30efed4faf13e3ac166a3cee705df
Mao771 <mao771@ukr.net>
2020-04-26 00:00:50 +0300

    Some changes

--- definitions.py
+++ definitions.py
@@ -1,3 +1,4 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+SETTINGS_FILE = os.path.join(ROOT_DIR, 'settings.conf')
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from util import DbHelper
-import os
 from sklearn.metrics import mean_squared_error
 from pandas import DataFrame
 from matplotlib import pyplot
@@ -8,10 +6,9 @@ from pandas.plotting import autocorrelation_plot
 import pandas as pd
 import warnings
 import math
-import numpy as np
 
-from functools import reduce
-
+from app.utils import get_preprocessed_data, create_dataset
+from detection.EWMADetector import EWMADetector
 warnings.filterwarnings("ignore")
 
 
@@ -118,66 +115,6 @@ def pow(n, i):
     return n * pow(n, i - 1)
 
 
-def get_attacks_info():
-    conf = os.path.abspath('settings.conf')
-    db_helper = DbHelper(conf)
-
-    # series_snort = db_helper.get(measurements='snort_log',
-    #                              aggregation='',
-    #                              fields=('destination', 'severity', 'type', 'protocol'))
-
-    series_attacks = db_helper.get(measurements='snort_log',
-                                   aggregation='',
-                                   fields=('destination', 'severity', 'detail', 'ruleid', 'type'))
-
-    series_attacks.time = pd.to_datetime(series_attacks.time).dt.strftime('%d.%m.%Y')
-    series_attacks.drop(['destination', 'severity', 'ruleid'], inplace=True, axis=1)
-    series_attacks.to_csv('series_attacks_raw.csv')
-
-    series_attacks = series_attacks.reset_index().pivot_table(index='time', columns='detail', aggfunc='sum')
-    series_attacks.to_csv('series_attacks.csv')
-    #grouped = series_attacks.groupby(['time', 'type']).sum()
-
-    # grouped.reindex()
-    # print(grouped.drop(columns=['severity', 'destination', 'protocol']))
-
-    # sns.relplot(x="time", y="attack_rate", col="align",
-    #            size="coherence", style="choice",
-    #             facet_kws=dict(sharex=False),
-    #             kind="line", legend="full", data=series_attacks)
-
-
-def get_preprocessed_data():
-    conf = os.path.abspath('settings.conf')
-    db_helper = DbHelper(conf)
-
-    series_net = db_helper.get(measurements='net',
-                               aggregation='WHERE interface=\'em0\' ORDER BY time desc LIMIT 200',
-                               fields=('packets_recv as pr',
-                                       'packets_sent as ps',
-                                       'bytes_recv as br',
-                                       'bytes_sent as bs'))
-
-    processed_df = series_net.sort_values(by='time')
-    processed_df.reset_index(drop=True, inplace=True)
-
-    non_negative_difference(processed_df, ('pr', 'ps', 'br', 'bs'))
-    # processed_df.pr = processed_df.pr.diff()
-    # processed_df.ps = processed_df.ps.diff()
-    # processed_df.br = processed_df.br.diff()
-    # processed_df.bs = processed_df.bs.diff()
-    processed_df.dropna(inplace=True)
-    return processed_df
-    #cumulative_average = processed_df.pr.expanding(2).mean()
-
-
-def non_negative_difference(df, *columns):
-    for col in columns:
-        df[col] = df[col].diff()
-        mean_value = df[col].where(df[col] > 0).dropna().mean()
-        df[col][df[col] < 0] = mean_value
-
-
 def lyapunov_method():
     preprocessed_df = get_preprocessed_data()
     pyplot.plot(preprocessed_df.pr)
@@ -190,57 +127,15 @@ def lyapunov_method():
     forecast(train, test, log=True)
 
 
-def create_dataset():
-    conf = os.path.abspath('settings.conf')
-    db_helper = DbHelper(conf)
-
-    series_net = db_helper.get(measurements='net',
-                               aggregation='WHERE interface=\'em0\'',
-                               fields=('packets_recv',
-                                       'packets_sent',
-                                       'bytes_recv',
-                                       'bytes_sent',
-                                       'drop_in',
-                                       'drop_out',
-                                       'err_in',
-                                       'err_out'))
-
-    series_cpu = db_helper.get(measurements='cpu',
-                               aggregation='WHERE cpu=\'cpu-total\'',
-                               fields=('usage_system', 'usage_user'))
-
-    series_mem = db_helper.get(measurements='mem',
-                               aggregation='',
-                               fields=('used_percent', 'buffered', 'cached'))
-
-    series_attacks = db_helper.get(measurements='snort_log',
-                                   aggregation='',
-                                   fields=('destination', 'severity', 'detail', 'ruleid', 'type'))
-
-    to_datetime(series_net, series_cpu, series_mem, series_attacks)
-    non_negative_difference(series_net, 'packets_recv',
-                            'packets_sent',
-                            'bytes_recv',
-                            'bytes_sent')
-
-    dfs = [series_net, series_cpu, series_mem, series_attacks]
-    df_final = reduce(lambda left, right: pd.merge(left, right, on='time', how='left'), dfs)
-    df_final['mark'] = np.where(pd.isna(df_final.type), 'normal', 'attack')
-    df_final.packets_recv.dropna()
-
-
-def to_datetime(*data_frames):
-    for df in data_frames:
-        df.time = pd.to_datetime(df.time).dt.strftime('%d-%m-%y %H:%M:%S')
-
-
 def replace_na(df, *columns):
     for col in columns:
         df[col][df[col].isna()] = df[col].dropna().mean()
 
 
 if __name__ == '__main__':
-    create_dataset()
+    ds = create_dataset()
+    ewma_detector = EWMADetector(ds.packets_recv)
+    ewma_detector.detect()
     # 'non_negative_difference(packets_recv) as pr',
     # 'non_negative_difference(packets_sent) as ps',
     # 'non_negative_difference(bytes_recv) as br',
